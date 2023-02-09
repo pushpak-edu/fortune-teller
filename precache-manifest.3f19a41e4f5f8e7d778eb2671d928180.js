a8e2953859ff339406199a02b4433ff89d18dd09
Pushpak Sharma <pushpaksharma@Pushpaks-Air.ht.home>
2023-02-09 10:50:10 +0530

    Updates

---
+++ precache-manifest.3f19a41e4f5f8e7d778eb2671d928180.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e65cddec2b6ee9d34b900c21759b8a3",
+    "url": "/fortune-teller/index.html"
+  },
+  {
+    "revision": "ea90d775cb2276ba9f3b",
+    "url": "/fortune-teller/static/css/main.81e6fef5.chunk.css"
+  },
+  {
+    "revision": "b57c39673f650a7a3894",
+    "url": "/fortune-teller/static/js/2.6f4f9af9.chunk.js"
+  },
+  {
+    "revision": "ea90d775cb2276ba9f3b",
+    "url": "/fortune-teller/static/js/main.be2f4d8c.chunk.js"
+  },
+  {
+    "revision": "ccb7eb5377acb0e23db7",
+    "url": "/fortune-teller/static/js/runtime~main.deeac6df.js"
+  }
+]);
